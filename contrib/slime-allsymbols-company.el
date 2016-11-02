67c63c0c89d1c5eaa13aefc7aaeea9a0bf2f8c78
Alan Ruttenberg <alanruttenberg@gmail.com>
2016-11-01 20:56:26 -0400

    Extension of slime-company (completions that popup as menu as you type) but with completion ranging over symbols from all packages. TODO: Add some other info such as if bound or fbound and use to sort by priority

---
+++ contrib/slime-allsymbols-company.el
@@ -0,0 +1,94 @@
+(require 'slime)
+(require 'cl-lib)
+
+;; based on slime-company
+
+(define-slime-contrib slime-allsymbols-company
+  "Use slime-company completion over all packages, and dabbrev-expand for tab completion"
+  (:license "GPL")
+  (:authors "Alan Ruttenberg")
+  (:slime-dependencies slime-company)
+  (:swank-dependencies slime-allsymbols-company)
+  (:on-load (slime-allsymbols-company--enable))
+  (:on-unload (slime-allsymbols-company--disable))
+  )
+
+;; doesn't like to be calle directly
+(defun dabbrev-expand-complete (&optional arg)
+  (interactive "*P")
+  (call-interactively 'dabbrev-expand))
+    
+(defun slime-allsymbols-company--enable ()
+  ;; remove c-p-c hooks. It gets loaded because of a dependency
+  (progn
+    (remove-hook 'slime-completion-at-point-functions
+		 #'slime-c-p-c-completion-at-point)
+    (remove-hook 'slime-connected-hook 'slime-c-p-c-on-connect)
+    (when (featurep 'slime-repl)
+	`((define-key slime-mode-map "\C-c\C-s"
+	    ',(lookup-key slime-mode-map "\C-c\C-s"))
+	  (define-key slime-repl-mode-map "\C-c\C-s"
+	    ',(lookup-key slime-repl-mode-map "\C-c\C-s")))))
+  (add-hook 'slime-completion-at-point-functions #'dabbrev-expand-complete)
+  (setq slime-completion-at-point-functions (subst 'dabbrev-expand-complete 'slime-simple-completion-at-point slime-completion-at-point-functions))
+  (setq company-backends (remove 'company-slime company-backends))
+  (dolist (h '(slime-mode-hook slime-repl-mode-hook sldb-mode-hook))
+    (remove-hook h 'slime-company-maybe-enable)
+    (add-hook h 'slime-allsymbols-company-maybe-enable)))
+
+(defun slime-allsymbols-company--disable ()
+  (setq company-backends (remove 'company-slime-allsymbols company-backends))
+  (dolist (h '(slime-mode-hook slime-repl-mode-hook sldb-mode-hook))
+    (remove-hook h 'slime-allsymbols-company-maybe-enable)))
+
+(defun slime-allsymbols-company-maybe-enable ()
+  (when (slime-company-active-p)
+    (company-mode 1)
+    (add-to-list 'company-backends 'company-slime-allsymbols)
+    (setq slime-company-completion 'allsymbol-complete)))
+
+(defun slime-company-disable ()
+  (setq company-backends (remove 'company-slime company-backends)))
+
+(defun slime-allsymbols-company--fetch-candidates-async (prefix)
+  (when (slime-connected-p)
+    (let ((slime-current-thread t))
+      (lexical-let ((package (slime-current-package))
+		    (prefix prefix))
+	(cons :async (lambda (callback)
+		       (lexical-let ((callback callback))
+			 (slime-eval-async
+			     `(swank:allsymbol-completions ,prefix ',package)
+			   (lambda (result)
+			     (funcall callback (car result))))
+			 package)))))))
+
+(defun company-slime-allsymbols (command &optional arg &rest ignored)
+  "Company mode backend for slime."
+  (cl-case command
+	   (init
+	    (slime-company-active-p))
+	   (prefix
+	    (when (and (slime-company-active-p)
+		       (slime-connected-p)
+		       (or slime-company-complete-in-comments-and-strings
+			   (null (company-in-string-or-comment))))
+	      (company-grab-symbol)))
+	   (candidates
+	    (slime-allsymbols-company--fetch-candidates-async (substring-no-properties arg)))
+	   (match
+	       (let ((prefix (company-grab-symbol) ))
+		 (+ (search prefix arg) (length prefix))))
+	   (meta
+	    (slime-company--arglist (substring-no-properties arg)))
+	   (annotation (concat " " (get-text-property 0 'flags arg)))
+	   (doc-buffer
+	    (slime-company--doc-buffer (substring-no-properties arg)))
+	   (location
+	    (slime-company--location (substring-no-properties arg)))
+	   (post-completion
+	    (slime-company--post-completion (substring-no-properties arg)))
+	   (no-cache t)
+	   (sorted t)))
+
+(provide 'slime-allsymbols-company)
